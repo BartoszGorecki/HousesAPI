7480815a2cc663f1f5c0a593e3a9733f6d21f722
Silverado <uninstallgame4444@gmail.com>
2019-09-27 20:42:18 +0200

    fixes func done

--- src/components/CreateHouse/index.js
+++ src/components/CreateHouse/index.js
@@ -25,10 +25,10 @@ class CreateHouse extends Component {
     e.preventDefault();
     const {address, area, owner, price} = this.state;
     const house = {
-      "address": address,
-      "area": area,
-      "owner": owner,
-      "price": price
+      address,
+      area,
+      owner,
+      price
     }
     axios.post(`${url}/houses`, house)
     .then(response => {
@@ -36,6 +36,12 @@ class CreateHouse extends Component {
         alert('Could not create a house!')
       } else {
         alert('House created successfully!')
+        this.setState({
+          address: '',
+          area: '',
+          owner: '',
+          price: ''
+        })
       } 
     })
     .catch(err => {
@@ -49,10 +55,10 @@ class CreateHouse extends Component {
       <>
         <button><Link to='/'>Back to Home</Link></button>
         <form onSubmit={this.createHouse}>
-          <input type='text' name='address' value={address} onChange={this.changeInput} placeholder='Address' />
-          <input type='number' name='area' value={area} onChange={this.changeInput} placeholder='Area' />
-          <input type='text' name='owner' value={owner} onChange={this.changeInput} placeholders='Owner' />
-          <input type='text' name='price' value={price} onChange={this.changeInput} placeholders='Price' />
+          <input type='text' name='address' value={address} onChange={this.changeInput} placeholder='Address' required minLength='5' />
+          <input type='number' name='area' value={area} onChange={this.changeInput} placeholder='Area' required/>
+          <input type='text' name='owner' value={owner} onChange={this.changeInput} placeholder='Owner' required minLength='5'/>
+          <input type='text' name='price' value={price} onChange={this.changeInput} placeholder='Price' required/>
           <button type='submit'>Add house</button>
         </form>
       </>
